fc1075b28377b546ac2fc1c067b072d810dedc99
El Cattivo <elcattivo@byteland.cc>
2016-03-23 17:11:59 +0100

    Add informational version to assembly manifest

--- CloudFlareUtilities/Properties/AssemblyInfo.cs
+++ CloudFlareUtilities/Properties/AssemblyInfo.cs
@@ -17,18 +17,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.1.0.0")]
 [assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyInformationalVersion("0.1.0.0-alpha")]
 
 [assembly:CLSCompliant(true)]
 [assembly:ComVisible(false)]
