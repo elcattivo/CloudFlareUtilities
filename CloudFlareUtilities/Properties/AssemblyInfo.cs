8af623ac62c878b8860d8227aca472c84fe4b403
El Cattivo <elcattivo@byteland.cc>
2016-04-18 00:47:39 +0200

    Update version to 0.2.1

--- CloudFlareUtilities/Properties/AssemblyInfo.cs
+++ CloudFlareUtilities/Properties/AssemblyInfo.cs
@@ -17,8 +17,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
 
-[assembly: AssemblyVersion("0.2.0.0")]
-[assembly: AssemblyFileVersion("0.2.0.0")]
+[assembly: AssemblyVersion("0.2.1.0")]
+[assembly: AssemblyFileVersion("0.2.1.0")]
 [assembly: AssemblyInformationalVersion("0.2.0-alpha")]
 
 [assembly:CLSCompliant(true)]
